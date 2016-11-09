f92df83a1c320979427f435a403da6b59563dc77
Hany Albeshry <hany@orangestudio.com>
2016-11-09 20:36:29 +0200

    Initial upload
    Initial upload

---
+++ lyrica/media/L_L_DPN_12/js/index.js
@@ -0,0 +1,30 @@
+$("body").swipe( {
+		swipe:function(event, direction, distance, duration, fingerCount) {
+		  if(direction == "top" || direction == "bottom"){
+				event.preventDefault();
+		  }
+		  var page = location.pathname.substr(0,  (location.pathname.lastIndexOf('/')));
+		  page = page.substr(page.lastIndexOf('/') + 1,  page.length);
+		  var pageIndex = parseInt(page.substr(page.lastIndexOf('_') + 1,  page.length));
+		  page = page.substr(0,  (page.lastIndexOf('_') + 1));
+		  
+		  if(direction == "right"){
+		  pageIndex--;
+		  //alert("../"+page + '' + ((pageIndex <= 9) ? ('0' + pageIndex) : pageIndex ) +"/index.html" );
+		  }
+		  if(direction == "left"){
+		  pageIndex++;
+		  //window.location = "../"+page + '' + ((pageIndex <= 9) ? ('0' + pageIndex) : pageIndex ) +"/index.html" ;
+		  //alert("../"+page + '' + ((pageIndex <= 9) ? ('0' + pageIndex) : pageIndex ) +"/index.html" );
+		  }
+		  window.location = "../"+page + '' + ((pageIndex <= 9) ? ('0' + pageIndex) : pageIndex ) +"/index.html" ;
+	},
+		 threshold:25,
+		 cancelThreshold:12,
+		 fallbackToMouseEvents: true,
+	});
+	$("img").click(function(){
+			$(this).trigger("touchstart");
+		});	
+		
+setTimeout(function(){$("#graphDiv").css("width", "620px")}, 2000);
